892aeb2e3a9b4c0e4c7e7cee42ac791fd9c511ec
Satya <satishdodake1993@gmail.com>
2019-10-26 16:49:40 +0530

    m

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2019 Company
+// </copyright>
+// <creator name="Satish Dodake"/>
+// --------------------------------------------------------------------------------------------------------------------
+namespace ObjectOriented.RegularExpresion
+{
+    using System;
+    using ObjectOriented.Commercialdata;
+    using ObjectOriented.InventoryManagement;
+    using ObjectOriented.StockReport;
+  
+    /// <summary>
+    /// this is Main Class .
+    /// </summary>
+    public class Program
+    {
+     /// <summary>
+    /// This main Method which is the entry point of execution of Application.
+    /// </summary>
+    /// <param name="args">it takes string type arguments</param>
+        public static void Main(string[] args)
+        {
+          ////this charector take choice from user.       
+            char again;
+
+            try
+            {
+                ////do while loop use to continue the operation
+                do
+                {
+                    ////here print the list of opeartion
+                    Console.WriteLine("**********************************");
+                    Console.WriteLine("1. Inventory Management Report \n" + "2. Inventory CURD Operation\n" + "3. Stock Portfolio\n" + "4. Commercial Proccessing\n" + "5. Regular Expression\n");
+                    Console.WriteLine("*********************************** ");
+
+                    ////here user enter theire choice to continue operation
+                    Console.WriteLine("Enter Your Choice To Run Operation ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+                    ////switch case use to perform different operation base on user choice
+                    switch (choice)
+                    {
+                        case 1:
+                            ////here call the object of utility to print inventory report
+                            Utility.InventoryManagementReport();
+                            break;
+                        case 2:
+                            ////here call the method with class name to perform curd operation
+                            InventoryCurdOperation.InventoryManagementCurdOpeartion();
+                            break;
+                        case 3:
+                            ////here print stock portfolio report
+                            StockPortfolio.StockReport();
+                            break;
+                        case 4:
+                            ////here perform account stock operation like buy share ,sell share.
+                            StockAccountOperation.StockAccountChoice();
+                            break;
+                        case 5:
+                            ////here check pattern
+                            RegularExpression.RegularExpressionChecker();
+                            break;
+                        default:
+                            ////this is invalide case
+                            Console.WriteLine("Please Enter Valide Number");
+                            break;
+                    }
+
+                    ////here asked to user Do continue or not.
+                    Console.WriteLine("\nDo You want To Continue the Press 'Y' Or 'N' ");
+                    again = Console.ReadLine()[0];
+                    if(again==' ')
+                    {
+                        Console.WriteLine("Please Enter Valide Input");
+                        break;
+                    }
+                }
+                while (again == 'Y' || again == 'y');
+
+                ////it hold the console untill press the key
+                Console.ReadKey();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please Enter Valide Input ..." + e);
+            }         
+        }
+    }
+}
---
+++ StockReport/Stock.cs
@@ -0,0 +1,65 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Stock.cs" company="Bridgelabz">
+//   Copyright © 2019 Company
+// </copyright>
+// <creator name="Satish Dodake"/>
+// ------------------------------------------------------------------------------------------------------
+namespace ObjectOriented.StockReport
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// this is stock class take stock input from user 
+    /// </summary>
+    public class Stock
+    {
+        /// <summary>
+        /// it hold name of share of stock
+        /// </summary>
+        public string nameOfShare;
+
+        /// <summary>
+        /// it hold number of share of stock
+        /// </summary>
+       public int numberOfShare;
+
+        /// <summary>
+        /// this hold price of share
+        /// </summary>
+       public double sharePrice;
+
+        /// <summary>
+        /// this method get stock details as input
+        /// </summary>
+        public void StockGetInput()
+        {
+            try
+            {
+                ////here enter name of stock
+                Console.WriteLine("Enter Name of Share/Stock ");
+                this.nameOfShare = Console.ReadLine();
+
+                ////here enter number of share
+                Console.WriteLine("Enter Number of Share ");
+                this.numberOfShare = Convert.ToInt32(Console.ReadLine());
+
+                ////enter price of share here
+                Console.WriteLine("Enter Price of Per Share ");
+                this.sharePrice = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (InvalidCastException i)
+            {
+                Console.WriteLine("Please Enter Valide Input " + i);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please Enter Valide Input" + e);
+            }
+          
+        }
+    }
+}
